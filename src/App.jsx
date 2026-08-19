0b8c8f7bbac9e9a9824cc2d48618f5f5c100aa75
Bela Gupta <bgupta4@ncsu.edu>
2026-08-19 17:59:32 -0400

    Added the Google Map embed for the location

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Hero from './components/Hero.jsx';
 import Register from './components/Register.jsx';
 import Sponsors from './components/Sponsors.jsx';
 import Faq from './components/Faq.jsx';
+import Location from './components/Location';
 import Footer from './components/Footer.jsx';
 
 export default function App() {
@@ -19,6 +20,7 @@ export default function App() {
       <Register />
       <Sponsors />
       <Faq />
+      <Location />
       <Footer />
       <Analytics />
     </>
