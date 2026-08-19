bdd8e28d12ed24f98e7940ca1fda1602d7d5763c
ArjunMamidala <arjunmamidala88@gmail.com>
2026-08-19 13:31:59 -0400

    Updated the banner to season 2027 and added the link to code of conduct

--- src/components/TrustBadge.jsx
+++ src/components/TrustBadge.jsx
@@ -1,14 +1,16 @@
-import siteConfig from '../data/siteConfig.js';
-
 export default function TrustBadge() {
-  const { year, region, color } = siteConfig.event.trustBadge;
-
-  const href = `https://mlh.io/seasons/${region}-${year}/events?utm_source=${region}-${year}&utm_medium=TrustBadge&utm_campaign=${region}-${year}&utm_content=${color}`;
-  const src = `https://s3.amazonaws.com/logged-assets/trust-badge/${year}/${color}.svg`;
-
   return (
-    <a id="mlh-trust-badge" href={href} target="_blank" rel="noreferrer">
-      <img src={src} alt={`Major League Hacking ${year} Hackathon Season`} style={{ width: '100%' }} />
+    <a
+      id="mlh-trust-badge"
+      href="https://mlh.io/na?utm_source=na-hackathon&utm_medium=TrustBadge&utm_campaign=2026-season&utm_content=white"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img
+        src="https://logged-assets.s3.amazonaws.com/trust-badge/2027/mlh-trust-badge-2027-white.svg"
+        alt="Major League Hacking 2026 Hackathon Season"
+        style={{ width: '100%' }}
+      />
     </a>
   );
 }
